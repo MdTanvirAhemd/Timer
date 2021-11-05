9f670d77b1b250ea80de5d8636dae3b8b4ed4e8d
Overlord <tanvir1005@student.sust.edu>
2021-11-06 00:25:33 +0600

    All Code Added

--- app/src/main/java/com/ahmed/timer/MainActivity.java
+++ app/src/main/java/com/ahmed/timer/MainActivity.java
@@ -3,12 +3,77 @@ package com.ahmed.timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart, btnPause, btnStop;
+
+    Timer myTimer;
+    TextView textView;
+    int x = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnPause = findViewById(R.id.btnPause);
+        btnStart = findViewById(R.id.btnStart);
+        btnStop  = findViewById(R.id.btnStop);
+        textView = findViewById(R.id.textView);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (myTimer!=null) {
+                    return;
+                }
+                myTimer = new Timer();
+                myTimer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView.setText(x + "");
+
+                                x++;
+                            }
+                        });
+
+
+                    }
+                },00,1000);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (myTimer!=null) {
+                    myTimer.cancel();
+                    myTimer = null;
+                }
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (myTimer!=null) {
+                    myTimer.cancel();
+                    myTimer = null;
+                }
+
+                x = 0;
+            }
+        });
     }
 }
